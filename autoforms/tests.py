83976ab25cc904a85caebd70c132ad4cef2bb005
jeffkit <jeff@fallever.com>
2010-12-11 20:22:30 +0800

    just a test

--- autoforms/tests.py
+++ autoforms/tests.py
@@ -1,9 +1,3 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
 
 from django.test import TestCase
 
