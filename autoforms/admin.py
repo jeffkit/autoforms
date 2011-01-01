f6775a318eba4f81c1956e97f0b99df52a55de21
jeffkit <jeff@fallever.com>
2011-01-01 23:22:33 +0800

    Add a csv export feature to form admin

--- autoforms/admin.py
+++ autoforms/admin.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response,get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
+from django.template import loader, Context
 
 from autoforms import models
 from autoforms import forms
@@ -22,17 +23,26 @@ class FormAdmin(admin.ModelAdmin):
 
     def preview(self,request,id):
         form = models.Form.objects.get(pk=id)
-        return render_to_response('autoforms/admin/form_preview.html',{'form':form,'title':_('Preview form : %(form_name)s'%{'form_name':form.name})},context_instance = RequestContext(request))
+        return render_to_response('autoforms/admin/form_preview.html',{'form':form,'title':_('Preview form : %(form_name)s')%{'form_name':form.name}},context_instance = RequestContext(request))
 
     def data(self,request,id):
         form = models.Form.objects.get(pk=id)
-        return render_to_response('autoforms/admin/form_data.html',{'form':form,'title':_('Data of form : %(form_name)s'%{'form_name':form.name})},context_instance = RequestContext(request))
+        return render_to_response('autoforms/admin/form_data.html',{'form':form,'title':_('Data of form : %(form_name)s')%{'form_name':form.name}},context_instance = RequestContext(request))
 
     def embed(self,request,id):
         return HttpResponse('get embed code')
 
     def export(self,request,id,format='csv'):
-        return HttpResponse('exporting')
+        form = models.Form.objects.get(pk=id)
+        if format == 'csv':
+            response = HttpResponse(mimetype='text/csv')
+            response['Content-Disposition'] = 'attachment; filename=%s.csv'%id
+            template = loader.get_template('autoforms/admin/form_export.csv')
+            context = Context({'datalist':form.search(),'fields':form.sorted_fields()})
+            response.write(template.render(context))
+            return response
+        else:
+            return HttpResponse('format not support yet')
 
     def get_urls(self):
         urls = super(FormAdmin,self).get_urls()
@@ -40,7 +50,7 @@ class FormAdmin(admin.ModelAdmin):
             (r'^(?P<id>\d+)/preview/$',self.admin_site.admin_view(self.preview)),
             (r'^(?P<id>\d+)/data/$',self.admin_site.admin_view(self.data)),
             (r'^(?P<id>\d+)/embed/$',self.admin_site.admin_view(self.embed)),
-            (r'^(?P<id>\d+)/data/export/$',self.admin_site.admin_view(self.export)),
+            (r'^(?P<id>\d+)/data/export/(?P<format>\w+)/$',self.admin_site.admin_view(self.export)),
         )
         return form_urls + urls
 
